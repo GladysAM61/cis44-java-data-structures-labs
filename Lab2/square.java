955ee89521d234d1a2dfff342abdd97466906cda
GladysAM61 <142520356+GladysAM61@users.noreply.github.com>
2026-09-11 21:42:35 -0700

    Create square.java

---
+++ Lab2/square.java
@@ -0,0 +1,12 @@
+package com.mycompany.lab2;
+
+/**
+ *
+ * @author gladysarias
+ */
+public class square extends quadrilateral{
+    //a square has all the same sides so we just add one side to both length and width
+    public square(double side){
+        super(side,side);
+    }
+}
