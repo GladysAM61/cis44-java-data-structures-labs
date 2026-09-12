abf1b56970a7ab55da5697a001972a9b70dcaeb2
GladysAM61 <142520356+GladysAM61@users.noreply.github.com>
2026-09-11 21:42:10 -0700

    Create rectangle.java

---
+++ Lab2/rectangle.java
@@ -0,0 +1,11 @@
+package com.mycompany.lab2;
+
+/**
+ *
+ * @author gladysarias
+ */
+public class rectangle extends quadrilateral{
+    public rectangle(double length,double width){
+        super(length,width);
+    }
+}
